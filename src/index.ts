a26d28bf64e278f17162280b8e573e579bd6fd94
Kosuke Isobe <naughtydog0721@gmail.com>
2020-01-21 16:32:26 +0900

    add ics

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import got from 'got';
+import ical from 'ical-generator';
+import mkdirp from 'mkdirp';
+import path from 'path';
+import rimraf from 'rimraf';
+
+interface Liver {
+  name: string;
+}
+
+interface Event {
+  id: number;
+  name: string;
+  description: string;
+  url: string;
+  start_date: string;
+  end_date: string;
+  livers: Liver[];
+}
+
+interface Data {
+  events: Event[];
+}
+
+interface Response {
+  data: Data;
+}
+
+rimraf.sync('public');
+mkdirp.sync('public');
+
+(async () => {
+  try {
+    const response = await got('https://api.itsukaralink.jp/v1.2/events.json');
+    const cal = ical({
+      domain: 'https://www.itsukaralink.jp/',
+      name: 'いつから.link',
+    });
+
+    const json = JSON.parse(response.body) as Response;
+    json.data.events.forEach(event => {
+      const livers = event.livers.map(liver => liver.name).join('\n');
+      cal.createEvent({
+        start: new Date(event.start_date),
+        end: new Date(event.end_date),
+        summary: event.name,
+        description: `${livers}\n\n${event.description}`,
+        url: event.url,
+      });
+    });
+
+    await fs.writeFileSync(path.join('public', 'itsukara.ics'), cal.toString());
+  } catch (error) {
+    console.error(error);
+  }
+})();
