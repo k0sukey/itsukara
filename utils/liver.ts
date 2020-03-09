85dd5ce6481edf260b1a6410b6c7bb96094bc697
Kosuke Isobe <naughtydog0721@gmail.com>
2020-03-09 11:45:56 +0900

    add liver

---
+++ utils/liver.ts
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.itsukaralink.jp/livers', {
+    waitUntil: 'networkidle0',
+  });
+
+  const data = await page.evaluate(() => {
+    const livers: Array<{
+      name: string;
+      thumbnail: string;
+      channel: string;
+    }> = [];
+    document.querySelectorAll<HTMLDivElement>('.liver').forEach(liver => {
+      const name = (liver.getElementsByClassName(
+        'liver-followBoxText',
+      )[0] as HTMLSpanElement).innerText;
+      const images = liver.getElementsByTagName('img');
+      const thumbnail = images[0].getAttribute('src') as string;
+      const channel = (images[2]
+        .parentElement as HTMLAnchorElement).getAttribute('href') as string;
+      livers.push({
+        name,
+        thumbnail,
+        channel,
+      });
+    });
+    return livers;
+  });
+  console.log(data);
+
+  await browser.close();
+})();
